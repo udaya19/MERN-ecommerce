e4bf8b0137fe327c7c6b27194cd36028db84e60e
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-10-24 12:35:14 +0530

    Search feature

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -1,7 +1,9 @@
 const Product = require("../models/products");
+const ApiFeatures = require("../utils/apiFeatures");
 exports.getAllProducts = async (req, res) => {
   try {
-    const products = await Product.find({});
+    const apiFeatures = new ApiFeatures(Product.find(), req.query).search();
+    const products = await apiFeatures.query;
     return res.json(200, {
       message: "Products fetched successfully",
       success: true,
