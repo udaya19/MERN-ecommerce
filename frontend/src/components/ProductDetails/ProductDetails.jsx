7cd07c58a4b7a7b2ee8e72be68c1285a782430fc
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-11-22 10:35:59 +0530

    Product details and review card

--- frontend/src/components/ProductDetails/ProductDetails.jsx
+++ frontend/src/components/ProductDetails/ProductDetails.jsx
@@ -1,17 +1,32 @@
 import React, { useEffect } from "react";
 import Carousel from "react-material-ui-carousel";
 import "./ProductDetails.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getProductDetails } from "../../api/product";
 import {
   getProductDetailsRequest,
   getProductDetailsSuccess,
 } from "../../redux/productDetailsSlice";
 import { useParams } from "react-router-dom";
+import ReactStars from "react-rating-stars-component";
+import ReviewCard from "./ReviewCard";
+import Loader from "../layout/Loader/Loader";
+// import Loader from "../layout/Loader/Loader";
 
 const ProductDetails = () => {
   const dispatch = useDispatch();
   const params = useParams();
+  const { product, loading, error } = useSelector(
+    (state) => state.productDetails
+  );
+  const option = {
+    edit: false,
+    color: "rgba(20,20,20,0.1)",
+    activeColor: "tomato",
+    value: product.ratings,
+    isHalf: true,
+    size: window.innerWidth < 600 ? 20 : 25,
+  };
   useEffect(() => {
     const getDetailsOfProduct = async () => {
       try {
@@ -19,19 +34,78 @@ const ProductDetails = () => {
         const response = await getProductDetails(params.id);
         dispatch(getProductDetailsSuccess(response.product));
       } catch (err) {
-        console.log(err);
+        console.log(error);
       }
     };
     getDetailsOfProduct();
-  }, [dispatch, params]);
+  }, [dispatch, params, error]);
   return (
-    <div>
-      <div className="ProductDetails">
-        <div>
-          <Carousel></Carousel>
-        </div>
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className="ProductDetails">
+            <div>
+              <Carousel>
+                {product.images &&
+                  product.images.map((item, i) => (
+                    <img
+                      className="CarouselImage"
+                      src={item.url}
+                      key={item.url}
+                      alt={`Slide ${i}`}
+                    />
+                  ))}
+              </Carousel>
+            </div>
+            <div>
+              <div className="detailsBlock-1">
+                <h2>{product.name}</h2>
+                <p>Product #{product._id}</p>
+              </div>
+              <div className="detailsBlock-2">
+                <ReactStars {...option} />
+                <span>({product.numOfReviews} Review)</span>
+              </div>
+              <div className="detailsBlock-3">
+                <h1>{`₹${product.price}`}</h1>
+                <div className="detailsBlock-3-1">
+                  <div className="detailsBlock-3-1-1">
+                    <button>-</button>
+                    <input type="number" value="1" />
+                    <button>+</button>
+                  </div>
+                  <button>Add to Cart</button>
+                </div>
+                <p>
+                  Status:{" "}
+                  <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                    {product.Stock < 1 ? "Out Of Stock" : "In Stock"}
+                  </b>
+                </p>
+              </div>
+              <div className="detailsBlock-4">
+                Description:<p>{product.description}</p>
+              </div>
+              <button className="submitReview">Submit Review</button>
+            </div>
+          </div>
+          <h3 className="reviewsHeading">REVIEWS</h3>
+          {product.reviews && product.reviews[0] ? (
+            <div className="reviews">
+              {product.reviews &&
+                product.reviews.map(
+                  (review) => <ReviewCard review={review} />,
+                  []
+                )}
+            </div>
+          ) : (
+            <p className="noReviews">No Reviews Yet</p>
+          )}
+        </>
+      )}
+    </>
   );
 };
 
---
+++ frontend/src/components/ProductDetails/ReviewCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+import profilePng from "../../images/Profile.png";
+const ReviewCard = ({ review }) => {
+  const option = {
+    edit: false,
+    color: "rgba(20,20,20,0.1)",
+    activeColor: "tomato",
+    value: review.rating,
+    isHalf: true,
+    size: window.innerWidth < 600 ? 20 : 25,
+  };
+  return (
+    <div className="reviewCard">
+      <img src={profilePng} alt="User" />
+      <p>{review.name}</p>
+      <ReactStars {...option} />
+      <span>{review.comment}</span>
+    </div>
+  );
+};
+
+export default ReviewCard;
